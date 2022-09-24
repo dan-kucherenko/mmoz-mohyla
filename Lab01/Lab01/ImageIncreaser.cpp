18203decac3ae2f5fc8fde82b803daf9564a4e3b
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-09-25 01:36:36 +0300

    Initial commit

---
+++ Lab01/Lab01/ImageIncreaser.cpp
@@ -0,0 +1,4 @@
+#include "ImageIncreaser.h"
+
+void increaseImageScale(const cv::Mat in_image, cv::Mat& out_image, int n) {
+}
---
+++ Lab01/Lab01/ImageIncreaser.h
@@ -0,0 +1,3 @@
+#include <opencv2/core/core.hpp>
+#pragma once
+void increaseImageScale(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
