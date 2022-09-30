770976317fdd0d141820c5062eab249f72e33949
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-10-01 00:16:13 +0300

    function name refactores in header file

--- Lab01/Lab01/ImageIncreaser.h
+++ Lab01/Lab01/ImageIncreaser.h
@@ -2,4 +2,4 @@
 #pragma once
 void increaseImageScale(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
 void increaseImageWithStep(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
-void addColorToEmptyPixels(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
+void addDataToEmptyPixels(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
