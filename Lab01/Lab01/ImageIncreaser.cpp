07bcc1dfbcea7c516c0a66b0c2a38c6c1109e0b6
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-10-01 19:13:18 +0300

    Bugs found and fixed

--- Lab01/Lab01/ImageIncreaser.cpp
+++ Lab01/Lab01/ImageIncreaser.cpp
@@ -7,35 +7,40 @@ void increaseImageWithStep(const cv::Mat in_image, cv::Mat& out_image, int n) {
 	}
 }
 void addDataToEmptyPixels(const cv::Mat in_image, cv::Mat& out_image, int n) {
-	for (int i = 0; i < out_image.rows; i++) {
-		for (int j = 0; j < out_image.cols; j++) {
+	for (int i = 1; i < out_image.rows; i++) {
+		for (int j = 1; j < out_image.cols; j++) {
 			if (out_image.data[i * out_image.cols + j] == 205) {
-				if (j == 0 || i == 0) {
-					if (out_image.data[i * out_image.cols + j - 1] != 205 && out_image.data[i * out_image.cols + j + 1] != 205)
-						out_image.data[i * out_image.cols + j] = (out_image.data[i * out_image.cols + j - 1] + out_image.data[i * out_image.cols + j + 1]) / 2;
-					else if (out_image.data[(i + 1) * out_image.cols + j] != 205 && out_image.data[(i - 1) * out_image.cols + j] != 205)
-						out_image.data[i * out_image.cols + j] = (out_image.data[(i - 1) * out_image.cols + j] + out_image.data[(i + 1) * out_image.cols + j]) / 2;
-				} else if (i == out_image.rows - 1) {
-					if (out_image.data[(i - 1) * out_image.cols + j - 1] != 205 && out_image.data[(i - 1) * out_image.cols + j + 1] != 205)
-						out_image.data[i * out_image.cols + j] = (out_image.data[(i - 1) * out_image.cols + j - 1] + out_image.data[(i - 1) * out_image.cols + j + 1]) / 2;
-				} else {
-					if (&out_image.data[(i + 1) * out_image.cols + j - 1] != nullptr && &out_image.data[(i - 1) * out_image.cols + j + 1] != nullptr &&
-						&out_image.data[(i + 1) * out_image.cols + j + 1] != nullptr && &out_image.data[(i - 1) * out_image.cols + j - 1] != nullptr)
-						out_image.data[i * out_image.cols + j] = (out_image.data[(i - 1) * out_image.cols + j - 1] +
-							out_image.data[(i - 1) * out_image.cols + j + 1] +
-							out_image.data[(i + 1) * out_image.cols + j - 1] + out_image.data[(i + 1) * out_image.cols + j + 1]) / 4;
-					if (out_image.data[i * out_image.cols + j - 1] != 205 && out_image.data[i * out_image.cols + j + 1] != 205)
-						out_image.data[i * out_image.cols + j] = (out_image.data[i * out_image.cols + j - 1] + out_image.data[i * out_image.cols + j + 1]) / 2;
-					if (out_image.data[(i + 1) * out_image.cols + j] != 205 && out_image.data[(i - 1) * out_image.cols + j] != 205)
-						out_image.data[i * out_image.cols + j] = (out_image.data[(i - 1) * out_image.cols + j] + out_image.data[(i + 1) * out_image.cols + j]) / 2;
-				}
+				if (&out_image.data[(i + 1) * out_image.cols + j - 1] != nullptr && &out_image.data[(i - 1) * out_image.cols + j + 1] != nullptr &&
+					&out_image.data[(i + 1) * out_image.cols + j + 1] != nullptr && &out_image.data[(i - 1) * out_image.cols + j - 1] != nullptr)
+					out_image.data[i * out_image.cols + j] = (out_image.data[(i - 1) * out_image.cols + j - 1] +
+						out_image.data[(i - 1) * out_image.cols + j + 1] +
+						out_image.data[(i + 1) * out_image.cols + j - 1] + out_image.data[(i + 1) * out_image.cols + j + 1]) / 4;
+				if (out_image.data[i * out_image.cols + j - 1] != 205 && out_image.data[i * out_image.cols + j + 1] != 205)
+					out_image.data[i * out_image.cols + j] = (out_image.data[i * out_image.cols + j - 1] + out_image.data[i * out_image.cols + j + 1]) / 2;
+				else if (out_image.data[(i + 1) * out_image.cols + j] != 205 && out_image.data[(i - 1) * out_image.cols + j] != 205)
+					out_image.data[i * out_image.cols + j] = (out_image.data[(i - 1) * out_image.cols + j] + out_image.data[(i + 1) * out_image.cols + j]) / 2;
 			}
 		}
 	}
+}
 
+void fillTheExtremePoints(const cv::Mat in_image, cv::Mat& out_image, int n) {
+	for (int i = 0; i < out_image.rows; i++) {
+		for (int j = 0; j < out_image.cols; j++) {
+			if (j == 0)
+				out_image.data[i * out_image.cols + j] = out_image.data[i * out_image.cols + j + 1];
+			else if (j == out_image.cols - 1)
+				out_image.data[i * out_image.cols + j] = out_image.data[i * out_image.cols + j - 1];
+			else if (i == 0)
+				out_image.data[i * out_image.cols + j] = out_image.data[(i + 1) * out_image.cols + j];
+			else if (i == out_image.rows - 1)
+				out_image.data[i * out_image.cols + j] = out_image.data[(i - 1) * out_image.cols + j];
+		}
+	}
 }
 
 void increaseImageScale(const cv::Mat in_image, cv::Mat& out_image, int n) {
 	increaseImageWithStep(in_image, out_image, n);
 	addDataToEmptyPixels(in_image, out_image, n);
+	fillTheExtremePoints(in_image, out_image, n);
 }
--- Lab01/Lab01/ImageIncreaser.h
+++ Lab01/Lab01/ImageIncreaser.h
@@ -3,3 +3,4 @@
 void increaseImageScale(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
 void increaseImageWithStep(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
 void addDataToEmptyPixels(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
+void fillTheExtremePoints(const cv::Mat in_image, cv::Mat& out_image, int n = 2);
