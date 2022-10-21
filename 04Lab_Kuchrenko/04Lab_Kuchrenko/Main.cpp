45fd98d467a95fc0f755fafbb6a26168a871b556
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-10-21 23:50:58 +0300

    Sobel filter implementation completed

--- 04Lab_Kuchrenko/04Lab_Kuchrenko/Main.cpp
+++ 04Lab_Kuchrenko/04Lab_Kuchrenko/Main.cpp
@@ -1,8 +1,25 @@
 ﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "SobelOperator.h"
 
 using namespace std;
+using namespace cv;
 
-int main(void)
-{
+int main(void) {
+	string str = "Image";
+	string imageName("D:/dev/MMOZ/Tif/Geneva.tif");
+	Mat image = imread(imageName, IMREAD_GRAYSCALE);
+	if (image.data == 0) {
+		cout << "Could not open or find the image" << endl;
+		return -1;
+	}
+	Mat sobel_image(image.rows, image.cols, CV_8UC1, Scalar(0));
+	Mat sobel_lib_image(image.rows, image.cols, CV_64F, Scalar(0));
+	sobel_operator(image, sobel_image);
+	Sobel(image, sobel_lib_image, -1, 1, 1);
+	imshow("Original image", image);
+	imshow("Sobel-filtered image", sobel_image);
+	imshow("Sobel-lib-filtered image", sobel_lib_image);
+	waitKey(0);
 	return 0;
 }
--- 04Lab_Kuchrenko/04Lab_Kuchrenko/SobelOperator.cpp
+++ 04Lab_Kuchrenko/04Lab_Kuchrenko/SobelOperator.cpp
@@ -1,4 +1,69 @@
 #include "SobelOperator.h"
+using namespace cv;
+void convolution(const Mat& in_image, const double* mask, int ksize, Mat& out_image) {
+	int sum = 0;
+	int* temp = new int[in_image.total()];
 
-void sobel_operator(const cv::Mat& in_image, cv::Mat& out_image) {
+	for (int i = 0; i < in_image.total(); i++)
+		temp[i] = in_image.data[i];
+
+	for (int i = ksize / 2; i < in_image.rows - ksize / 2; i++) {
+		for (int j = 0; j < in_image.cols - ksize / 2; j++) {
+			sum = 0;
+			for (int a = 0; a < ksize; a++) {
+				for (int b = 0; b < ksize; b++) {
+					sum += in_image.data[(i + a - ksize / 2) * in_image.cols + j - b - ksize / 2] * mask[a * ksize + b];
+				}
+			}
+			temp[i * in_image.cols + j] = sum;
+		}
+	}
+	for (int i = 0; i < in_image.rows * in_image.cols; i++) {
+		int val = temp[i];
+		if (val < 0)
+			val = 0;
+		if (val > 255)
+			val = 255;
+		out_image.data[i] = val;
+	}
+	delete[]temp;
+}
+
+void gaussian_blur(const Mat& in_image, Mat& out_image, const int ksize, double sigma) {
+	const double exp = 2.71828, pi = 3.14159;
+	double sum = 0;
+	double* mask = new double[ksize * ksize];
+	int y = -ksize / 2;
+	for (int i = 0; i < ksize; i++) {
+		int x = -ksize / 2;
+		for (int j = 0; j < ksize; j++) {
+			mask[i * ksize + j] = 1 / (2 * pi * pow(sigma, 2)) * pow(exp, -((pow(x, 2) + pow(y, 2)) / (2 * pow(sigma, 2))));
+			sum += mask[i * ksize + j];
+			x++;
+		}
+		y++;
+	}
+	convolution(in_image, mask, ksize, out_image);
+	delete[] mask;
+}
+
+void sobel_operator(const Mat& in_image, Mat& out_image) {
+	Mat temp = in_image.clone();
+	gaussian_blur(in_image, temp, 3, 0.8);
+	const double* sobel_xmask = new double[9] { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+	const double* sobel_ymask = new double[9] { -1, -2, 1, 0, 0, 0, -1, 2, 1 };
+
+	Mat g_x(in_image.rows, in_image.cols, CV_16S, Scalar(0));
+	Mat g_y(in_image.rows, in_image.cols, CV_16S, Scalar(0));
+	convolution(temp, sobel_xmask, 3, g_x);
+	convolution(temp, sobel_ymask, 3, g_y);
+
+	for (size_t i = 0; i < in_image.rows; i++) {
+		for (size_t j = 0; j < in_image.cols; j++) {
+			out_image.data[i * in_image.cols + j] = sqrt(pow(g_x.data[i * in_image.cols + j], 2)
+				+ pow(g_y.data[i * in_image.cols + j], 2));
+		}
+	}
+	delete[]sobel_xmask;
+	delete[]sobel_ymask;
 }
