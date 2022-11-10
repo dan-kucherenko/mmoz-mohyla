a4803423e82e30ed420d5e45a3ce9660f5544f36
dan_kucherenko <daniil.ukraine@gmail.com>
2022-11-10 18:04:41 +0200

    Implementing the region growth algorithm

---
+++ 07_Lab_Kucherenko/07_Lab_Kucherenko/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Region_Growth.h"
+
+using namespace std;
+using namespace cv;
+
+int main(void) {
+	string str = "Image";
+	string imageName("C:/dev/MMOZ/Tif/IM11.tif");
+	Mat image = imread(imageName, IMREAD_GRAYSCALE);
+	Mat marked_image(image.rows, image.cols, CV_8UC1, Scalar(0));
+	if (image.data == 0) {
+		cout << "Could not open or find the image" << endl;
+		return -1;
+	}
+	
+	imshow("Input image", image);
+	imshow("Image after marking alg.", marked_image);
+	waitKey(0);
+	return 0;
+}
---
+++ 07_Lab_Kucherenko/07_Lab_Kucherenko/Region_Growth.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <opencv2/core/mat.hpp>
+void region_growth(const cv::Mat& input, cv::Mat& output);
