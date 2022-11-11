667a7335307ea1d63288e78570931d5b7352ffd2
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-11-11 10:13:36 +0200

    Temp commit

--- 07_Lab_Kucherenko/07_Lab_Kucherenko/Region_Growth.h
+++ 07_Lab_Kucherenko/07_Lab_Kucherenko/Region_Growth.h
@@ -1,3 +1,3 @@
 #pragma once
 #include <opencv2/core/mat.hpp>
-void region_growth(const cv::Mat& input, cv::Mat& output);
+void region_growth(const cv::Mat& input, cv::Mat& output, unsigned char root_pixel_br, int pixel_threshold = 128);
