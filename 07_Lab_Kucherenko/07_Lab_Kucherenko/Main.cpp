b7737b5788ccdb3f0903d0ef890f27444e807c44
dan_kucherenko <daniil.ukraine@gmail.com>
2022-11-13 16:17:06 +0200

    Fixes to the algorithm for not only linear neighbours

--- 07_Lab_Kucherenko/07_Lab_Kucherenko/Main.cpp
+++ 07_Lab_Kucherenko/07_Lab_Kucherenko/Main.cpp
@@ -8,15 +8,19 @@ using namespace cv;
 int main(void) {
 	string str = "Image";
 	string imageName("C:/dev/MMOZ/Tif/IM11.tif");
+	//string imageName("C:/dev/MMOZ/Tif/contour.tif");
 	Mat image = imread(imageName, IMREAD_GRAYSCALE);
 	Mat marked_image(image.rows, image.cols, CV_8UC1, Scalar(0));
+	Mat marked_image_coord(image.rows, image.cols, CV_8UC1, Scalar(0));
 	if (image.data == 0) {
 		cout << "Could not open or find the image" << endl;
 		return -1;
 	}
-	region_growth(image, marked_image, 20, 15);
+	region_growth(image, marked_image, 128, 100);
+	region_growth(image, marked_image_coord, 150, 200, 80);
 	imshow("Input image", image);
 	imshow("Image after marking alg.", marked_image);
+	imshow("Image (coord) after marking alg.", marked_image_coord);
 	waitKey(0);
 	return 0;
 }
--- 07_Lab_Kucherenko/07_Lab_Kucherenko/Region_Growth.h
+++ 07_Lab_Kucherenko/07_Lab_Kucherenko/Region_Growth.h
@@ -1,3 +1,3 @@
 #pragma once
-#include <opencv2/core/mat.hpp>
 void region_growth(const cv::Mat& input, cv::Mat& output, unsigned char root_pixel_br, int pixel_threshold = 128);
+void region_growth(const cv::Mat& input, cv::Mat& output, size_t pixel_x, size_t pixel_y, int pixel_threshold = 128);
